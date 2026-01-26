8859d9f62da862364a401fcaf56d0b6fddf7615e
jjminsuh <minsuh3203@gmail.com>
2026-01-26 16:29:40 +0000

    feat: concept expansion agent 실행 코드 추가

---
+++ core/tests/test_concept_expansion_agent.py
@@ -0,0 +1,60 @@
+# python -m core.tests.test_concept_expansion_agent
+
+import json
+from typing import cast
+from dotenv import load_dotenv
+
+from core.agents.concept_expansion_agent import ConceptExpansionAgent
+from core.contracts.curriculum import CurriculumGraph
+from core.graphs.state_definition import CreateCurriculumOverallState
+from core.llm.solar_pro_2_llm import get_solar_model
+
+def main():
+    # 1. env 로드
+    load_dotenv()
+
+    # 2. LLM 생성
+    llm = get_solar_model(
+        model_name="solar-pro2",
+        temperature=0.5
+    )
+
+    # 3. Agent 생성
+    agent = ConceptExpansionAgent(llm)
+
+    # 4. 테스트용 JSON 데이터 로드
+    with open("./dummy_data/dummy_initial_curriculum.json", "r", encoding="utf-8") as f:
+        curriculum = json.load(f)
+
+    curriculum_graph: CurriculumGraph = cast(CurriculumGraph, curriculum)
+    reason = "비전공자인 사용자 수준에 비해 세부 학습 키워드가 부족함."
+    
+    dummy_state: CreateCurriculumOverallState = {
+        # ===== Input State =====
+        "paper_name": None,
+        "paper_summary": None,
+        "initial_keywords": [],
+        "paper_content": None,
+        "user_info": None,
+
+        # ===== Output State (초기에는 비어있거나 None 성격) =====
+        "final_curriculum": None,
+        
+        "keyword_subgraph": None,
+        "curriculum": curriculum_graph,
+        "is_keyword_sufficient": False,
+        "is_resource_sufficient": False,
+        "current_iteration_count": 0,
+        "keyword_expand_reason": reason
+    }
+    
+    # 5. Agent 실행
+    result = agent.run(dummy_state)
+
+    # # 6. 결과 출력
+    print("===== Concept Expansion Result =====")
+    print(json.dumps(result, indent=2, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()
